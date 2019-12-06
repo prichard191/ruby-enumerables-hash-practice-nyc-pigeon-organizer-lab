3ea5796ca7e46d6945187dc14985de1875410534
Patrick Richard <prichard191@gmail.com>
2019-12-06 15:10:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ organized = {}
+  data.each do |trait, o|
+   o.each do |option, pigeons|
+    pigeons.each do |pigeon|
+      organized[pigeon] ||= {}
+      organized[pigeon][trait] ||= []
+      organized[pigeon][trait] << option.to_s
+       end
+   end
+ end
+
+ organized
 end
